13304372cb2b6bef63789ffa3ae6b470b3c852e0
ymcho0827 <skyjjang1234@gmail.com>
2017-10-13 15:12:45 -0400

    Create success.php

---
+++ success.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>redirecting</title>
+<meta http-equiv="refresh" content="3; URL=https://freshrents.co//">
+<meta name="keywords" content="automatic redirection">
+</head>
+<body>
+Request Quote has been sent!
+Thank you for requesting your contribute form!!<br><br>
+If your browser doesn't automatically go there within a few seconds, 
+you may want to go to 
+<a href="https://freshrents.co//">the destination</a> 
+manually.
+</body>
+</html>
