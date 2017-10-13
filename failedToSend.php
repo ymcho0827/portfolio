ee7c09933a225167ad3eaeee1653f0ad2557c735
ymcho0827 <skyjjang1234@gmail.com>
2017-10-13 15:13:03 -0400

    Create failedToSend.php

---
+++ failedToSend.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>redirecting</title>
+<meta http-equiv="refresh" content="3; URL=contributeForm.php">
+<meta name="keywords" content="automatic redirection">
+</head>
+<body>
+Request Quote has been failed sent...
+Redirecting to Contribute Form<br><br>
+If your browser doesn't automatically go there within a few seconds, 
+you may want to go to 
+<a href="contributeForm.php">the destination</a> 
+manually.
+</body>
+</html>
